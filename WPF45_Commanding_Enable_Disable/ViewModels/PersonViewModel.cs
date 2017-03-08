629de33678b5acae1c4ee6c6186ab4fde49e21a3
Adam Gruer <abbot_grover@hotmail.com>
2017-03-09 09:44:29 +1100

    added relay command class

---
+++ WPF45_Commanding_Enable_Disable/ViewModels/PersonViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF45_Commanding_Enable_Disable.ViewModels
+{
+    class PersonViewModel
+    {
+    }
+}
