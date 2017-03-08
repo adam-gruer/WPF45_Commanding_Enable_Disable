abaa2a362be6f24525358dd4663420e48946df21
Adam Gruer <abbot_grover@hotmail.com>
2017-03-09 08:56:45 +1100

    added modelClasses

---
+++ WPF45_Commanding_Enable_Disable/ModelClasses/DataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WPF45_Commanding_Enable_Disable.ModelClasses
+{
+    /// <summary>
+    /// The DataAccess class
+    /// </summary>
+    public class DataAccess
+    {
+        public ObservableCollection<PersonInfo> GetPersonData()
+        {
+            return new PersonsDataStore();
+        }
+    }
+
+}
